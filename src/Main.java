ec87a5dd5f6dd9671df68a6584453bb48582d549
isfanzamonov <101178567+isfanzamonov@users.noreply.github.com>
2022-03-30 13:43:04 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,42 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in); //scanner for taking in user inputs
-        System.out.println("1. Input manually");
-        System.out.println("2. Input from file");
-        Integer num = userInput.nextInt();
-        if (num == 2) {
-            FileHandler fh = new FileHandler("FileWriterFile.txt");
-            ArrayList<String> readInLines = new ArrayList<>();
-            int lineAmount;
-            lineAmount = fh.linesInFile();
-            Database db = new Database("FileWriterFile.txt", lineAmount);
-            ArrayList<Integer> padding = new ArrayList<>();
-            db.appendRecord(db.getPadding(), lineAmount);
+   public static void main(String[] args) {
+       Scanner userInput = new Scanner(System.in); //scanner for taking in user inputs
+       System.out.println("1. Input manually");
+       System.out.println("2. Input from file");
+       Integer num = userInput.nextInt();
+       if (num == 2) {
+           FileHandler fh = new FileHandler("FileWriterFile.txt");
+           Database db = new Database("FileWriterFile.txt");
+       } else {
+           //NEED TO TRY/CATCH CREATION OF FILE BUT LIMIT THE PATH TO JUST THIS FOLDER - TO USE ON ANY COMPUTER
+       }
+       FileHandler fh = new FileHandler("FileWriterFile.txt");
+       Database db = new Database("FileWriterFile.txt");
+       System.out.println("1. Input more manually");
+       System.out.println("2. Display");
+       num = userInput.nextInt();
+       if (num == 1) {
+           ArrayList<String> appendInput = new ArrayList<>();
+           appendInput.add("Creativity");
+           appendInput.add("Museum");
+           appendInput.add("3");
+           db.appendRecord(db.getPadding(), appendInput, true);
+       } else {
+           System.out.println(db.getPadding());
+           System.out.println(db.getRecord(1)); //NOT READING PROPERLY
+           System.out.println(db.getRecord(2));
+           System.out.println(db.getRecord(3));
+           db.getCasType(1); //NOT WORKING BECAUSE GETRECORD IS BROKEN
+           db.getActivity(2);
+           db.getActivity(3);
+       }
 
-        }
-    }
+   }
 
 }
+
