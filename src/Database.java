83bc9d7860e5c02d77a3bb332d9254630a50f158
isfanzamonov <101178567+isfanzamonov@users.noreply.github.com>
2022-03-31 20:38:04 +0100

    First Working Program - Proof of Concept

--- src/Database.java
+++ src/Database.java
@@ -1,62 +1,77 @@
-import java.io.File;
 import java.util.ArrayList;
 
 public class Database {
 
-   private String filename;
-   private ArrayList<Integer> padding = new ArrayList<>();
+    private String filename; //creates holding variables for ease of use
+    private ArrayList<Integer> padding = new ArrayList<>();
 
-   public Database(String filename) {
-       this.filename = filename;
-   }
+    public Database(String filename) {
 
-   public ArrayList<Integer> getPadding() {
-       String[] temp;
-       temp = FileHandler.readFromFile(0).split(" ");
-       padding.add(Integer.parseInt(temp[0].replaceAll(" ", "")));
-       padding.add(Integer.parseInt(temp[1].replaceAll(" ", "")));
-       padding.add(Integer.parseInt(temp[2].replaceAll(" ", "")));
-       return padding;
-   }
+        this.filename = filename;
+        FileHandler fh = new FileHandler(filename);
 
-   public String getRecord(int rowNumber) {
-       getPadding();
-       if (rowNumber == 0) {
-           return FileHandler.readFromFile(0);
-       } else {
-           int start = FileHandler.readFromFile(0).length(); //PADDING LINE ACCOUNTED FOR
-           start = start + ((padding.get(0) + padding.get(1) + padding.get(2)) * (rowNumber - 1)) + (rowNumber * 2); //ADDS UP THE LENGTHS OF THE LINES BEFORE IT (IGNORES IF ROWNUMBER IS 1 BECAUSE PADDING LINE ALREADY ACCOUNTED FOR, ROWNUMBER * 2, TAKES INTO ACCOUNT EMPTY SPACES
-           return FileHandler.readFromFile(start) ;
-       }
-   }
+    }
 
-   public String getCasType(int rowNumber) {
-       return getRecord(rowNumber).substring(0, 14);
-   }
+    public ArrayList<Integer> getPadding() { //gets padding data from line 0
 
-   public String getActivity(int rowNumber) {
-       return getRecord(rowNumber).substring(14, 29);
-   }
+        String[] temp; //holds data as spaces removes from line 0, leaving only the padding numbers
+        temp = FileHandler.readFromFile(0).split(" "); //takes the separated padding numbers, includes spaces for now
+        padding.add(Integer.parseInt(temp[0].replaceAll(" ", ""))); //each one has spaces removed and is converted to integers for arithmetic
+        padding.add(Integer.parseInt(temp[1].replaceAll(" ", "")));
+        padding.add(Integer.parseInt(temp[2].replaceAll(" ", "")));
+        return padding; //returns an arraylist of the padding data
 
-   //public String getHours(int rowNumber) {
-   //  return getRecord(rowNumber).;
-   //}
+    }
 
-   public void appendRecord(ArrayList padding, ArrayList input, boolean append) {
-       if (append == true) {
-           String newString = "";
-           for (int i = 0; i < padding.size(); i++) {
-               newString = newString + input.get(i);
-               for (int j = 0; j < (Integer) padding.get(i) - input.get(i).toString().length(); j++) { //have to convert from object to string to int, there has to be a quicker way
-                   newString = newString + " ";
-               }
-           }
-           System.out.println(newString);
-           FileHandler.writeToFile(filename, newString, true);
-       }
-   }
+    public String getRecord(int rowNumber) {
 
-   public void deleteRecord(int rowNumber) {
-   }
+        getPadding();
+
+        if (rowNumber == 0) { //if padding line requested
+
+            return FileHandler.readFromFile(0); // returns first line
+
+        } else { //if any other line
+
+            int start = FileHandler.readFromFile(0).length(); //accounts for the padding line and moves to start of line 1
+            start = start + ((padding.get(0) + padding.get(1) + padding.get(2)) * (rowNumber - 1)) + (rowNumber * 2); //adds up lengths of lines before desired record to move to the start of that line
+            //only does this operation when rownumber > 0 to skip padding line, (rowNumber * 2) takes the end padding into account
+            return FileHandler.readFromFile(start).replace(".", ""); //searches for the record and removes the fullstop at the end
+
+        }
+
+    }
+
+    public String getCasType(int rowNumber) {
+        return getRecord(rowNumber).substring(0, padding.get(0) - 1); //returns only first segment from the record, the cas type
+    }
+
+    public String getActivity(int rowNumber) {
+        return getRecord(rowNumber).substring(padding.get(0), padding.get(0) + padding.get(1) - 1); //returns only second segment from the record, the activity
+    }
+
+    public String getHours(int rowNumber) {
+      return getRecord(rowNumber).substring(padding.get(0) + padding.get(1), padding.get(0) + padding.get(1) + padding.get(2) - 1); //returns last segment, the hours
+    }
+
+    public void appendRecord(ArrayList input, boolean append) {
+
+        padding = getPadding();
+
+        if (append == true) {
+            String newString = "";
+            for (int i = 0; i < padding.size(); i++) {
+                newString = newString + input.get(i);
+                for (int j = 0; j < padding.get(i) - input.get(i).toString().length(); j++) { //have to convert from object to string to int, there has to be a quicker way
+                    newString = newString + " ";
+                }
+            }
+            FileHandler.writeToFile(filename, newString + ".", true); //adds fullstop to avoid losing padding spaces at the end of lines when text file is saved
+        }
+
+    }
+
+    public void deleteRecord(int rowNumber) { //will create at a later date
+    }
 
 }
